4f597dec3efe8eb413b7955155a6f75245c36275
yuiwong <yuiwong@126.com>
2015-05-28 16:30:28 +0800

    add showYesOrNoMsg

--- common/utils/MsgPopupUtilsInterface.java
+++ common/utils/MsgPopupUtilsInterface.java
@@ -3,4 +3,15 @@ package nocom.common.utils;
 
 public interface MsgPopupUtilsInterface {
 	public void showOkMsg (String title, String msg);
+
+
+	public
+		void
+		showYesOrNoMsg (
+			String title,
+			String msg,
+			String yesTxt,
+			String noTxt,
+			android.content.DialogInterface.OnClickListener yes,
+			android.content.DialogInterface.OnClickListener no);
 }
