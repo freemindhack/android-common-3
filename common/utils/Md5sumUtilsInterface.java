88ca439b80dbd5f48e659a2a60c0f810014eb0c4
yuiwong <yuiwong@126.com>
2015-05-28 16:31:00 +0800

    add Md5sumUtils

---
+++ common/utils/Md5sumUtilsInterface.java
@@ -0,0 +1,21 @@
+package nocom.common.utils;
+
+
+import java.io.UnsupportedEncodingException;
+
+
+public interface Md5sumUtilsInterface {
+	public byte[] md5sumString (byte[] byteString,
+		int count, boolean to32Bytes)
+		throws UnsupportedEncodingException;
+
+
+	public byte[] md5sumString (String string);
+
+
+	public String md5sumStringHexString (byte[] byteString,
+		int count);
+
+
+	public String md5sumStringHexString (String string);
+}
