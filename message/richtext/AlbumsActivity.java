f73c08b2b5e25aa4aba8359c6b1dd3ab54277fee
yuiwong <yuiwong@126.com>
2015-07-28 20:05:04 +0800

    fixed land

--- message/richtext/AlbumsActivity.java
+++ message/richtext/AlbumsActivity.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,9 +17,11 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
 import com.za.smartlock.manufacturer.R;
+import common.utils.UIUtils;
 
 
 public class AlbumsActivity extends Activity {
@@ -28,13 +31,34 @@ public class AlbumsActivity extends Activity {
 		Log.v(TAG, "onCreate");
 
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_add_picture);
+		UIUtils.transparentStatus(getWindow());
+		UIUtils.transparentNavigation(getWindow());
+		setContentView(R.layout.activity_albums);
 
 		this.helper = new AlbumHelper();
 		this.helper.prepare(getApplicationContext());
 
 		this.initData();
 		this.initView();
+	}
+
+
+	@Override
+	public void onResume () {
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT
+			&& UIUtils.checkDeviceHasNavigationBar(this)
+			&& UIUtils.isPortrait(getApplicationContext())) {
+			tvAADummyNavigation.setVisibility(View.VISIBLE);
+		} else {
+			tvAADummyNavigation.setVisibility(View.GONE);
+		}
+
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+			UIUtils.dotNavigation(getWindow());
+		}
+
+		super.onResume();
 	}
 
 
@@ -50,6 +74,15 @@ public class AlbumsActivity extends Activity {
 
 	private void initView () {
 		Log.v(TAG, "initView");
+
+		TextView tvAADummyStatusbar = (TextView) findViewById(R.id.tvAADummyStatusbar);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+			tvAADummyStatusbar.setVisibility(View.VISIBLE);
+		} else {
+			tvAADummyStatusbar.setVisibility(View.GONE);
+		}
+
+		tvAADummyNavigation = (TextView) findViewById(R.id.tvAADummyNavigation);
 
 		RelativeLayout relativeLayoutAAPCancel = (RelativeLayout) findViewById(R.id.relativeLayoutAAPCancel);
 		relativeLayoutAAPCancel.setOnClickListener(new OnClickListener() {
@@ -106,4 +139,6 @@ public class AlbumsActivity extends Activity {
 	private ImageBucketAdapter adapter;
 
 	private AlbumHelper helper;
+
+	private TextView tvAADummyNavigation;
 }
